f3a4dc44f669052c4a8395f7f54d9d65a4a6e6bf
aymanalarousy-rgb <aymanalarousy@gmail.com>
2026-01-11 18:15:53 -0800

    Update appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.25d541d09c80460e857df8f99e498182',
+  appId: 'com.ayman.currencycompanion',
   appName: 'أسعار العملات ليبيا',
-  webDir: 'dist',
-  server: {
-    url: 'https://25d541d0-9c80-460e-857d-f8f99e498182.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
